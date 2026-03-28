1c122cac7f5d8a17be052106e3a7547ab82503b2
Patryk <s29443@pjwstk.edu.pl>
2026-03-28 20:26:00 +0100

    Dodaj klasę Person po której dziedziczą Employee i Student

---
+++ Wypożyczalnia_sprzętu/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Wypożyczalnia_sprzętu;
+
+public abstract class Person
+{
+    public Guid Id { get; private set; } = Guid.NewGuid();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public abstract int MaxActiveRentals { get; }
+    public abstract string UserType { get; }
+}
