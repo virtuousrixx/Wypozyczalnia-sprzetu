470ebd47183be2b5e7db4063c0619916b68ba5bb
Patryk <s29443@pjwstk.edu.pl>
2026-03-28 20:12:46 +0100

    Dodaj klasę Equipment zapewniającą działanie klas dziedziczących

---
+++ Wypożyczalnia_sprzętu/Equipment.cs
@@ -0,0 +1,11 @@
+﻿namespace Wypożyczalnia_sprzętu;
+
+    public abstract class Equipment
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public bool IsAvailable { get; private set; } = true;
+
+        public void MarkUnavailable() => IsAvailable = false;
+        public void MarkAvailable() => IsAvailable = true;
+    }
