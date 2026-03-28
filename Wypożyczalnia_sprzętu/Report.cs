4ecd6dc718bed9e3c16f639824983c655c65172a
Patryk <s29443@pjwstk.edu.pl>
2026-03-28 20:33:49 +0100

    Dodaj klasę Report, która wyświetla raport wypożyczeń sprzętu

---
+++ Wypożyczalnia_sprzętu/Report.cs
@@ -0,0 +1,15 @@
+﻿namespace Wypożyczalnia_sprzętu;
+
+public class Report
+{
+    public void PrintReport(List<Equipment> equipment, IEnumerable<Rent> rentals)
+    {
+        Console.WriteLine("Raport:");
+
+        Console.WriteLine($"Liczba sprzętu: {equipment.Count}");
+        Console.WriteLine($"Dostępny sprzęt: {equipment.Count(e => e.IsAvailable)}");
+        Console.WriteLine($"Liczba wypożyczonego sprzętu: {equipment.Count(e => !e.IsAvailable)}");
+        Console.WriteLine($"Aktywne wypożyczenia: {rentals.Count(r => !r.IsReturned)}");
+        Console.WriteLine($"Przeterminowane wypożyczenia: {rentals.Count(r => !r.IsReturned && r.DueDate < DateTime.Now)}");
+    }
+}
